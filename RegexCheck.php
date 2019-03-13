7b5dc7cc7d689d2021a7c7077ad5ad88de566fab
Daniel Alejandro <DBuelna45@gmail.com>
2019-03-12 18:58:29 -0700

    forgot to add quotes to define param

--- RegexCheck.php
+++ RegexCheck.php
@@ -16,31 +16,29 @@
  **/
 
 //one or more digits
-define(INT_REGEX, "/^\d+$/");
+define('INT_REGEX', "/^\d+$/");
 //floating point numbers, scientific notation included, positive/negative/unspecified allowed
-define(FLOAT_REGEX, "/[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/");
+define('FLOAT_REGEX', "/[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/");
 //alphanumeric, one or more characters
-define(ALPHA_NUM_REGEX, "/[A-z0-9]+/");
+define('ALPHA_NUM_REGEX', "/[A-z0-9]+/");
 //alphanumeric with underscores
-define(USERNAME_REGEX, "/[A-z0-9_\.]{1,28}/");
+define('USERNAME_REGEX', "/[A-z0-9_\.]{1,28}/");
 //password is 12-15 characters in length, the 32nd to 127th characters in the ASCII table are allowed (I normally encode in utf-8)
 //I strip leading/trailing whitespace on my inputs before doing anything with the passwords, by the way, by the way
-define(PASSWORD_REGEX, "/[\x20-\x7F]{12,55}/");
+define('PASSWORD_REGEX', "/[\x20-\x7F]{12,55}/");
 //first and last name.  I suppose this will also work validating ONLY first/last names as well.  Names like "Juan Carlos" are hard to parse...
-define(NAME_REGEX, "/[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+/");
+define('NAME_REGEX', "/[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+/");
 //American address, finish later
-define(ADDRESS_REGEX, "/(.*)/");
+define('ADDRESS_REGEX', "/(.*)/");
 //international address, finish later
-define(ADDRESS_INT_REGEX, "/(.*)/");
+define('ADDRESS_INT_REGEX', "/(.*)/");
 //email, finish later
-define(EMAIL_REGEX, "/(.*)/");
+define('EMAIL_REGEX', "/(.*)/");
 //American phone numbers, finish later
-define(PHONE_REGEX, "/(.*)/");
+define('PHONE_REGEX', "/(.*)/");
 //phone (international), finish later
-define(PHONE_INT_REGEX, "/(.*)/");
-//html tags
-define(HTML_REGEX, "/(.*)/");
-//script tags
-define(SCRIPT_REGEX, "/\<script(.*?)?\>(.|\s)*?\<\/script\>/");
-//URL regex
-define(URL_REGEX,"([-a-z0-9+.]*(:|:\/\/))?([\w_-]+\.)+[a-zA-Z]{2,}[-%\$_.+!*'(),;\/?:@=&\w#]*");
+define('PHONE_INT_REGEX', "/(.*)/");
+//html tags, finish later
+define('HTML_REGEX', "/(.*)/");
+define('SCRIPT_REGEX', "/\<script(.*?)?\>(.|\s)*?\<\/script\>/");
+define('URL_REGEX',"([-a-z0-9+.]*(:|:\/\/))?([\w_-]+\.)+[a-zA-Z]{2,}[-%\$_.+!*'(),;\/?:@=&\w#]*");
